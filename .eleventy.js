76e4cf7d71998de28ae9ac7ced54962ba5372044
Makeademic <kaplanwizard@gmail.com>
2025-05-01 15:26:25 +0000

    Amended library in .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.setBrowserSyncConfig({
+    // setup markdown to use emoji plugin
+  const markdownItEmoji = require("markdown-it-emoji");
+  eleventyConfig.amendLibrary("md", mdLib => mdLib.use(markdownItEmoji));
+  
+  eleventyConfig.setBrowserSyncConfig({
       files: './public/static/**/*.css',
     });
   
